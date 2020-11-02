ab2d82b542d89a8cfbb2778880d7304fe6a0b375
Garam Ann <maraqann@gmail.com>
2020-11-02 16:26:13 +0900

    DDL에 PK로 사용할 idx 추가 후 auto_increment=1로 설정

--- Queries.sql
+++ Queries.sql
@@ -1,8 +1,10 @@
 # 명함 테이블 생성
-CREATE TABLE `carddb`.`businesscard` (
-  `name` VARCHAR(20) NOT NULL,
-  `phone` VARCHAR(45) NOT NULL,
-  `company_name` VARCHAR(45) NULL,
-  `create_date` DATE NULL)
-ENGINE = MEMORY
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `businesscard` (
+  `idx` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `company_name` varchar(30) NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idx`),
+  UNIQUE KEY `idx_UNIQUE` (`idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
